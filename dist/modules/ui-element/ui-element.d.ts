3f1c8960b2c2f02545b31ced0a78fc90837c1b3c
Rhyan Eduardo <67023605+Rhyan-eduardo@users.noreply.github.com>
2026-08-26 05:43:43 -0300

    patch : added uicustom to uielement type

--- dist/modules/ui-element/ui-element.d.ts
+++ dist/modules/ui-element/ui-element.d.ts
@@ -1,8 +1,9 @@
 import { UIButton } from "../ui-button/ui-button.js";
+import { UICustom } from "../ui-custom/ui-custom.js";
 import { UIGroup } from "../ui-group/ui-group.js";
 import { UIIconButton } from "../ui-icon-button/ui-icon-button.js";
 import { UIIcon } from "../ui-icon/ui-icon.js";
 import { UIImage } from "../ui-image/ui-image.js";
 import { UILabel } from "../ui-label/ui-label.js";
 import { UIPanel } from "../ui-panel/ui-panel.js";
-export type UIElement = UIPanel | UIButton | UILabel | UIImage | UIIcon | UIGroup | UIIconButton;
+export type UIElement = UIPanel | UIButton | UILabel | UIImage | UIIcon | UIGroup | UIIconButton | UICustom;
--- src/modules/ui-element/ui-element.ts
+++ src/modules/ui-element/ui-element.ts
@@ -2,6 +2,7 @@
 // UI ELEMENT : 
 
 import { UIButton } from "../ui-button/ui-button.js";
+import { UICustom } from "../ui-custom/ui-custom.js";
 import { UIGroup } from "../ui-group/ui-group.js";
 import { UIIconButton } from "../ui-icon-button/ui-icon-button.js";
 import { UIIcon } from "../ui-icon/ui-icon.js";
@@ -9,4 +10,4 @@ import { UIImage } from "../ui-image/ui-image.js";
 import { UILabel } from "../ui-label/ui-label.js";
 import { UIPanel } from "../ui-panel/ui-panel.js";
 
-export type UIElement = UIPanel | UIButton | UILabel | UIImage | UIIcon | UIGroup | UIIconButton;
+export type UIElement = UIPanel | UIButton | UILabel | UIImage | UIIcon | UIGroup | UIIconButton | UICustom;
